b0b74767587b2366ca51021bd25d889e3fc2ac55
defyisvox <69298928+defyisvox@users.noreply.github.com>
2020-09-07 21:54:22 -0700

    Add files via upload
    Basic things you can do in R

---
+++ Simple R commands.R
@@ -0,0 +1,45 @@
+# create a variable (capitalization matters)
+# can be assigned with "=" or "<-" 
+x = 11
+y <- 7
+
+# calling a variable using 'print' or just typing the variable
+
+print(x)
+y
+
+# variables can be named with numbers in them, but not as a first character
+x.1 <- 14
+
+# unusable variable name
+1.x <- 13
+
+# data types are a thing in R, quoted numbers vs unquoted
+yy <- "1"
+yyy <- 1
+
+# basic math operators are usable + - / * 
+x + y
+x - y
+x * y
+x / y
+
+# exponents are usable
+x^y
+x^y + y^x
+
+# sqrt for square roots
+sqrt(y)
+y^(1/2)
+
+# logarithm
+log(y)
+
+# exponent of y / antilog
+exp(y)
+?exp
+
+?log2
+log2(y)
+
+abs(-14)
